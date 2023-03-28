e291949c232bcf6af17843aa27d2bcd7078b29b8
THIAGO305 <thiagofarias0df@gmail.com>
2023-03-28 18:43:04 -0300

    Validação com joi

--- api/app.js
+++ api/app.js
@@ -1,10 +1,21 @@
 const bcrypt = require('bcrypt')
+const Joi = require('joi')
 const express = require('express')
+
+const validator = require('express-joi-validation').createValidator({ passError: true })
+
 const app = express()
 
 app.use(express.json())
 
 const { deleteUser, insertUser } = require('./db')
+
+const userSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    is_shaver: Joi.boolean().required(),
+})
 
 app.get('/welcome', function (req, res) {
     res.json({ message: 'Hello Ola QA' })
@@ -17,7 +28,7 @@ app.delete('/user/:email', async function (req, res) {
     res.status(204).end()
 })
 
-app.post('/user', async function (req, res) {
+app.post('/user', validator.body(userSchema), async function (req, res) {
     const { name, email, password, is_shaver } = req.body
     const hashPass = await bcrypt.hash(password, 8)
 
@@ -28,12 +39,8 @@ app.post('/user', async function (req, res) {
         is_shaver: is_shaver
     }
 
-    // debito técnico porque ainda não consegue validar o campo shaver da forma correta
-    if (!user.name || !user.email || !user.password) {
-        return res.status(400).json({message: 'Every field is mandatory.'})
-    }
-
     console.log(user)
+
     try {
         await deleteUser(user.email)
         const id = await insertUser(user)
@@ -44,5 +51,18 @@ app.post('/user', async function (req, res) {
     }
 
 })
+
+app.use((err, req, res, next) => {
+    if (err && err.error && err.error.isJoi) {
+        // we had a joi error, let's return a custom 400 json response
+        res.status(400).json({
+            type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+            message: err.error.toString()
+        });
+    } else {
+        // pass on to another error handler
+        next(err);
+    }
+});
 
 app.listen(5000)
